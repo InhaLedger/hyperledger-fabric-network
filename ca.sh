ff9729f6513df8e6ec512e36a24adaba6c7ff184
Dong-Hyeon-Yu <ydh9516@gmail.com>
2022-11-01 17:36:52 +0900

    add script code to register orgs to CAs

--- ca.sh
+++ ca.sh
@@ -1,23 +1,8 @@
 #!/bin/bash
 
 
-#ROOTDIR=$(cd "$(dirname "$0")" && pwd)
-#export PATH=${ROOTDIR}/bin:${PWD}/bin:$PATH
-    #export FABRIC_CFG_PATH=${PWD}/configtx
-    #export VERBOSE=false
-
-# push to the required directory & set a trap to go back if needed
-#pushd ${ROOTDIR} > /dev/null
-#trap "popd > /dev/null" EXIT
-
-#. scripts/utils.sh  #import
-
-#: ${CONTAINER_CLI:="docker"}
-#: ${CONTAINER_CLI_COMPOSE:="${CONTAINER_CLI}-compose"}
-#infoln "Using ${CONTAINER_CLI} and ${CONTAINER_CLI_COMPOSE}"
-
-
 . env.sh #import
+
 
 function checkPrereqs() {
 
@@ -70,6 +55,76 @@ function caUp() {
 }
 
 
+
+# Before you can bring up a network, each organization needs to generate the crypto
+# material that will define that organization on the network. Because Hyperledger
+# Fabric is a permissioned blockchain, each node and user on the network needs to
+# use certificates and keys to sign and verify its actions. In addition, each user
+# needs to belong to an organization that is recognized as a member of the network.
+# You can use the Cryptogen tool or Fabric CAs to generate the organization crypto
+# material.
+
+# By default, the sample network uses cryptogen. Cryptogen is a tool that is
+# meant for development and testing that can quickly create the certificates and keys
+# that can be consumed by a Fabric network. The cryptogen tool consumes a series
+# of configuration files for each organization in the "organizations/cryptogen"
+# directory. Cryptogen uses the files to generate the crypto  material for each
+# org in the "organizations" directory.
+
+# You can also use Fabric CAs to generate the crypto material. CAs sign the certificates
+# and keys that they generate to create a valid root of trust for each organization.
+# The script uses Docker Compose to bring up three CAs, one for each peer organization
+# and the ordering organization. The configuration file for creating the Fabric CA
+# servers are in the "organizations/fabric-ca" directory. Within the same directory,
+# the "registerEnroll.sh" script uses the Fabric CA client to create the identities,
+# certificates, and MSP folders that are needed to create the test network in the
+# "organizations/ordererOrganizations" directory.
+
+# Create Organization crypto material using cryptogen or CAs
+function createOrgs() {
+  if [ -d "organizations/peerOrganizations" ]; then
+    rm -Rf organizations/peerOrganizations && rm -Rf organizations/ordererOrganizations
+  fi
+
+  # Create crypto material using Fabric CA
+    infoln "Generating certificates using Fabric CA"
+    ${CONTAINER_CLI_COMPOSE} -f compose/$COMPOSE_FILE_CA -f compose/$CONTAINER_CLI/${CONTAINER_CLI}-$COMPOSE_FILE_CA up -d 2>&1
+
+    . organizations/fabric-ca/registerEnroll.sh
+
+    while :
+    do
+      if [ ! -f "organizations/fabric-ca/org1/tls-cert.pem" ]; then
+        sleep 1
+      else
+        break
+      fi
+    done
+
+    infoln "Creating Org1 Identities"
+
+    createOrg1
+
+
+    infoln "Creating Orderer Org Identities"
+
+    createOrderer
+ 
+
+  infoln "Generating CCP files for Org1"
+  ./organizations/ccp-generate.sh
+}
+
+
+# Are we generating crypto material with this command?
+if [ ! -d "organizations/peerOrganizations" ]; then
+  CRYPTO_MODE="with crypto from '${CRYPTO}'"
+else
+  CRYPTO_MODE=""
+fi
+
+
+
 # when terminate CAs, default is not to remove fabric ca artifacts
 CLEAR="false"
 
@@ -117,6 +172,7 @@ done
 if [ "$MODE" == "up" ]; then
   infoln "Starting CAs using ${CRYPTO}"
   caUp
+  createOrgs # register orgs to CAs including admin users.
 elif [ "$MODE" == "down" ]; then
   infoln "Stopping ca"
   caDown

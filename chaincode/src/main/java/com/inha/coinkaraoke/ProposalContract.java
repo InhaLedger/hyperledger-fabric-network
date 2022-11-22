19df4719289adaa3d4436d22610c4827984be5cd
Dong-Hyeon-Yu <ydh9516@gmail.com>
2022-11-22 16:05:10 +0900

    [refactor] proposal contract 주석 추가

--- chaincode/src/main/java/com/inha/coinkaraoke/ProposalContract.java
+++ chaincode/src/main/java/com/inha/coinkaraoke/ProposalContract.java
@@ -8,8 +8,12 @@ import com.inha.coinkaraoke.ledgerApi.impl.ProposalServiceImpl;
 import org.hyperledger.fabric.contract.Context;
 import org.hyperledger.fabric.contract.ContractInterface;
 import org.hyperledger.fabric.contract.annotation.Contract;
+import org.hyperledger.fabric.contract.annotation.Info;
 
-@Contract
+@Contract(name = "ProposalContract",
+        info = @Info(title = "Proposal contract",
+                description = "managing proposal for editing and voting",
+                version = "0.0.1"))
 public class ProposalContract implements ContractInterface {
 
     private final ProposalService proposalService;
--- chaincode/src/main/java/com/inha/coinkaraoke/ledgerApi/ProposalService.java
+++ chaincode/src/main/java/com/inha/coinkaraoke/ledgerApi/ProposalService.java
@@ -6,6 +6,13 @@ import org.hyperledger.fabric.contract.Context;
 
 public interface ProposalService {
 
-    Proposal createProposal(final Context ctx, String userId, String type, Long timestamp, Stake stake);
+    /**
+     * @param proposerId subjectCN of the X.509 certificate.
+     * @param type proposal type. refer to board type at the application level.
+     * @param timestamp proposal timestamp.
+     * @param stake required to propose edit.
+     * @return {@link Proposal}
+     */
+    Proposal createProposal(final Context ctx, String proposerId, String type, Long timestamp, Stake stake);
 
 }
